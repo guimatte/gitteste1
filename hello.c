4a80cdb1a0ddd4de89cd311dcbddb4c3013c4f90
gui <guimatte2009@gmail.com>
2026-08-17 16:45:25 -0300

    Revert "comentado"
    This reverts commit 9da2e781682936c95eb4b8c9b668d94dd5429aae.

--- hello.c
+++ hello.c
@@ -1,8 +1,8 @@
   #include<stdio.h>
   
   int main(){
-    //int ret;
-    //ret = soma(2, 3);
-    //printf("O resultado é: %d", ret);
-    //return 0;
+    int ret;
+    ret = soma(2, 3);
+    printf("O resultado é: %d", ret);
+    return 0;
   }
